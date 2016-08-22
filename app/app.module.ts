2e51a7bfa9fa3e0c609bd7d6e16b322cc9005cb5
chibieee <gregneo21@gmail.com>
2016-08-21 20:17:04 -0400

    added search service/spotify api get

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,18 +1,30 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 import { routing } from './app.routes';
 
 import { AppComponent }  from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { AboutComponent } from './components/about/about.component';
 import { SearchComponent } from './components/search/search.component';
+import { SpotifyService } from './services/spotify.service';
+
 @NgModule({
-  imports: [ BrowserModule, routing ],
+  imports: [ 
+    BrowserModule,
+    routing,
+    FormsModule,
+    HttpModule
+     ],
   declarations: [
      AppComponent,
      NavbarComponent,
      AboutComponent,
      SearchComponent
+      ],
+      providers: [
+        SpotifyService
       ],
   bootstrap: [ AppComponent ]
 })
--- app/components/search/search.component.ts
+++ app/components/search/search.component.ts
@@ -1,12 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+
+import { SpotifyService } from '../../services/spotify.service';
 
 @Component({
     moduleId: module.id,
     selector: 'search',
-    templateUrl: 'search.component.html'
+    templateUrl: 'search.component.html',
+    providers: [ SpotifyService ]
 })
 export class SearchComponent implements OnInit {
-    constructor() { }
+    searchStr: string;
+
+    constructor(private spotifyService: SpotifyService) {
+
+     }
 
     ngOnInit() { }
+
+    searchMusic() {
+        this.spotifyService.searchMusic(this.searchStr)
+            .subscribe(res => {
+                console.log(res.artists.items);
+            })
+
+    }
 }
